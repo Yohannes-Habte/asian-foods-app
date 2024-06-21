2487346669408b025dedb711699bba76ddf6a2c1
mdMehediBillah <mailbillah@gmail.com>
2024-06-21 11:06:29 +0200

    data fetched from database

--- client/src/components/products/smallProductCart/SmallProductCart.jsx
+++ client/src/components/products/smallProductCart/SmallProductCart.jsx
@@ -1,61 +1,43 @@
-/* eslint-disable react/prop-types */
 import { Link } from "react-router-dom";
 import "./SmallProductCart.css";
-// import { useContext } from "react";
-// import { CartContext } from "../../../context/cart/CartProvider";
-// import { CART_ACTION } from "../../../context/cart/CartReducer";
-// import { toast } from "react-toastify";
+
 import { FaCartPlus } from "react-icons/fa";
 
 const SmallProductCart = ({ food }) => {
-  console.log("food:", food)
-  // const { cartItems, dispatch } = useContext(CartContext);
+  console.log("food:", food);
 
-  // Add to cart
-  // const addToCartHandler = async (id) => {
-  //   const existingItem = cartItems.find((item) => item.sys.id === id);
-
-  //   const quantity = existingItem ? existingItem.quantity + 1 : 1;
-
-  //   if (existingItem) {
-  //     toast.warning("Item exist in the cart!");
-  //   } else {
-  //     dispatch({
-  //       type: CART_ACTION.ADD_ITEM_TO_CART,
-  //       payload: { ...car, quantity },
-  //     });
-
-  //     toast.success("Item added to cart successfully!");
-  //   }
-  // };
-
-  const { food_id, name, description, price, country, spicy, image } = food;
-  // const {
-  //   fields: { brand, description, model, price, year, image },
-  //   sys: { id },
-  // } = car;
-
-  // const carDescription = description.content[0].content[0].value;
-  // const shortDescription = carDescription.slice(0, 139);
-  // const shortText = shortDescription.concat("...");
+  const {
+    food_id,
+    food_name,
+    description,
+    food_price,
+    country,
+    spicelevel,
+    image,
+  } = food;
 
   return (
     <>
-      <section className="cart-product-container">
-        <Link to={`/products/${food_id}`}>
-          <figure>
-            <img className="car-image" src={image} alt={name} />
-          </figure>
-          <h3 className="header-smallCard"> {name} </h3>
-          <p className="sub-title-text"> {country} </p>
-          <p> Spicy level: {spicy} </p>
-          <p>
-            {" "}
-            {description} <span className="text-red-500">read more</span>{" "}
-          </p>
-        </Link>
+      <section className="cart-product-container flex flex-col justify-between">
+        <div>
+          <Link to={`/products/${food_id}`}>
+            <figure>
+              <img className="car-image" src={image} alt={food_name} />
+            </figure>
+            <h3 className="header-smallCard"> {food_name} </h3>
+            <p className="sub-title-text uppercase"> {country} </p>
+            <p> Spicy level: {spicelevel} </p>
+            <p className="line-clamp-4">
+              {" "}
+              {description} <span className="text-red-500">read more</span>{" "}
+            </p>
+          </Link>
+        </div>
         <div className="flex justify-between mt-6" key={food_id}>
-          <p className="bg-gray-200 py-1 px-2 rounded"> Price: ${price} </p>
+          <p className="bg-gray-200 py-1 px-2 rounded">
+            {" "}
+            Price: ${food_price}{" "}
+          </p>
           <Link>
             <div className="p-2 bg-cyan-200">
               <FaCartPlus />
@@ -63,28 +45,6 @@ const SmallProductCart = ({ food }) => {
           </Link>
         </div>
       </section>
-      {/* <section className="cart-product-container">
-      <Link to={`/products/${id}`}>
-        <figure>
-          <img className="car-image" src={food.image} alt={food.name} />
-        </figure>
-        <h3 className="header-smallCard"> {brand} </h3>
-        <p className="sub-title-text"> {model} </p>
-        <p> Year: {year.slice(0, 4)} </p>
-        <p>
-          {" "}
-          {shortText} <span className="text-red-500">read more</span>{" "}
-        </p>
-      </Link>
-      <div className="flex justify-between mt-6">
-        <p className="bg-gray-200 py-1 px-2 rounded"> Price: ${price} </p>
-        <Link>
-          <div className="card-icon">
-            <FaCartPlus onClick={() => addToCartHandler(id)} />
-          </div>
-        </Link>
-      </div>
-    </section> */}
     </>
   );
 };
